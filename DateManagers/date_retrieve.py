007f1c44c73e0164956850dbde2564ac1deaff60
liangk12 <kenneth.liang12@gmail.com>
2018-12-02 21:31:30 -0500

    added logging o/p for DateRetriever funcs, unittests still pass. Creating initial dirs for the Student info

--- DateManagers/date_retrieve.py
+++ DateManagers/date_retrieve.py
@@ -5,6 +5,8 @@ Description: handles getting available dates and their days based on month, year
 import datetime, calendar
 import logging
 
+logging.getLogger().setLevel(logging.INFO)
+
 WEEKDAY_TO_INT = {
     "M" : 0, "T": 1, "W": 2, "TH": 3, "F": 4, "SAT": 5, "SUN": 6
 }
@@ -21,9 +23,10 @@ class DateRetriever(object):
     def parseSelectDays(self, selectDaysToParse):
         selDays = ["M", "T", "W", "TH", "F"]  # not incl weekends for now
         if not selectDaysToParse:
-            logging.warning("*** Getting Mon.-Fri. weekdays! ***")
+            logging.warning("*** Getting Mon.-Fri. weekdays! *** \n")
         else:
             selDays = selectDaysToParse.upper().split(",")
+            logging.info("*** Getting %s \n", str(selDays))
         return selDays
 
     def getSelectDaysAsInt(self):
@@ -34,11 +37,15 @@ class DateRetriever(object):
         allDates = calendar.monthrange(self.year, self.month)
         # idx 1 gets count of all the days; do +1 in range to make last day inclusive
         allDates = tuple(datetime.datetime(self.year, self.month, d) for d in range(1, allDates[1] + 1))
+        logging.info("\t there are %d days for month %d, year %d \n",
+                     len(allDates), self.month, self.year)
         return allDates
 
     def getDatesForSelectDays(self):
         selectDates = self.allDates
         selectDates = tuple((a.weekday(), a) for a in selectDates if a.weekday() in self.selectDaysAsInts)
+        logging.info("\t %d select days for month %d, year %d \n",
+                     len(selectDates), self.month, self.year)
         return selectDates
 
 
